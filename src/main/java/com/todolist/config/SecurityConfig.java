fd8adc8e03ade36d17b0e7b93853c1169e7b66b9
SeolA-Lee <dlcodus1201@naver.com>
2025-04-22 23:41:17 +0900

    feat: SecurityConfig authenticationEntryPoint() 추가

--- src/main/java/com/todolist/config/SecurityConfig.java
+++ src/main/java/com/todolist/config/SecurityConfig.java
@@ -1,16 +1,20 @@
 package com.todolist.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.todolist.exception.dto.ExceptionResponse;
 import com.todolist.jwt.JwtAuthenticationFilter;
 import com.todolist.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -20,6 +24,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final ObjectMapper objectMapper;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -30,8 +35,21 @@ public class SecurityConfig {
                         .requestMatchers("/member/register", "/member/login") // 회원가입, 로그인 시 요청 허가
                         .permitAll()
                         .anyRequest().authenticated())
+                .exceptionHandling(e -> e.authenticationEntryPoint(authenticationEntryPoint()))
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint() {
+        return (request, response, auth) -> {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType("application/json; charset=utf-8");
+
+            ExceptionResponse body = new ExceptionResponse(HttpStatus.UNAUTHORIZED.toString(), "로그인이 필요합니다.");
+            String json = objectMapper.writeValueAsString(body);
+            response.getWriter().write(json);
+        };
     }
 
     @Bean
